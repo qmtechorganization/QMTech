26a8fbc6c539cab9f404c9d026feb3c46889f13a
Sirallens <ing.jorge.eduardo.le@gmail.com>
2024-08-07 04:56:58 -0500

    Initial Components creation

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,81 @@
+<script>
+    let mobileNavOpen = false;
+  </script>
+  
+  <div>
+    <nav class="relative p-4 bg-black">
+      <div class="flex items-center justify-between">
+        <div class="flex items-center gap-6">
+          <a href="#" class="inline-block">
+            <img class="w-48" src="https://static.shuffle.dev/uploads/files/ec/ec578d7e076a0dfd4b92738b77ade3a71968172c/QM-Logo.png" alt="">
+          </a>
+          <ul class="hidden lg:flex items-center gap-2">
+            <li><a href="#" class="nav-menu inline-block py-2 px-3 hover:bg-gray-900 transition duration-200 text-white text-sm font-medium tracking-tight rounded-full">Services</a></li>
+            <li><a href="#" class="nav-menu inline-block py-2 px-3 hover:bg-gray-900 transition duration-200 text-white text-sm font-medium tracking-tight rounded-full">Our Work</a></li>
+            <li><a href="#" class="nav-menu inline-block py-2 px-3 hover:bg-gray-900 transition duration-200 text-white text-sm font-medium tracking-tight rounded-full">About Us</a></li>
+            <li><a href="#" class="nav-menu">FAQs</a></li>
+          </ul>
+        </div>
+        <a href="#" class="rounded-full border border-gray-200 bg-white px-5 py-3 h-14 hover:bg-gray-50 focus:ring-4 focus:ring-gray-200 hidden lg:inline-flex items-center justify-center gap-2 transition duration-200">
+          <span class="text-sm font-semibold tracking-tight text-black">Get A Quote</span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <path d="M14 6.66666H7.33333C4.38781 6.66666 2 9.05447 2 12V13.3333M14 6.66666L10 10.6667M14 6.66666L10 2.66666" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+          </svg>
+        </a>
+        <a on:click={() => mobileNavOpen = !mobileNavOpen} href="#" class="lg:hidden">
+          <svg class="navbar-burger text-white" width="51" height="51" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="56" height="56" rx="28" fill="currentColor"></rect>
+            <path d="M37 32H19M37 24H19" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+          </svg>
+        </a>
+      </div>
+    </nav>
+    <div class={`fixed top-0 left-0 bottom-0 w-5/6 max-w-xs z-50 ${mobileNavOpen ? 'block' : 'hidden'}`}>
+      <div on:click={() => mobileNavOpen = !mobileNavOpen} class="fixed inset-0 bg-black opacity-20"></div>
+      <nav class="relative p-8 w-full h-full bg-white overflow-y-auto">
+        <div class="flex items-center justify-between">
+          <a href="#" class="inline-block">
+            <img class="h-7" src="consulty-assets/logos/consulty-logo2.svg" alt="">
+          </a>
+          <a on:click={() => mobileNavOpen = !mobileNavOpen} href="#">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M6 18L18 6M6 6L18 18" stroke="#111827" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            </svg>
+          </a>
+        </div>
+        <ul class="flex flex-col gap-12 py-12">
+          <li>
+            <a href="#" class="inline-block py-2 px-3 hover:bg-gray-50 transition duration-200 rounded-full">
+              <div class="flex items-center gap-2">
+                <span class="text-sm font-medium tracking-tight">Products</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" fill="none">
+                  <path d="M12.848 6L8.18132 10.6667L3.51465 6" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+              </div>
+            </a>
+          </li>
+          <li><a href="#" class="inline-block py-2 px-3 hover:bg-gray-50 transition duration-200 text-sm font-medium tracking-tight rounded-full">Case Studies</a></li>
+          <li><a href="#" class="inline-block py-2 px-3 hover:bg-gray-50 transition duration-200 text-sm font-medium tracking-tight rounded-full">Support</a></li>
+          <li>
+            <a href="#" class="inline-block py-2 px-3 hover:bg-gray-50 transition duration-200 rounded-full">
+              <div class="flex items-center gap-2">
+                <span class="text-sm font-medium tracking-tight">Resources</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" fill="none">
+                  <path d="M12.848 6L8.18132 10.6667L3.51465 6" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <div class="flex flex-col gap-4">
+          <a href="#" class="rounded-full bg-black px-5 py-3 h-14 hover:bg-orange-600 focus:bg-orange-500 focus:ring-4 focus:ring-orange-200 inline-flex items-center justify-center gap-2 transition duration-200">
+            <span class="text-white text-sm font-semibold tracking-tight">Get A Free Consultation</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+              <path d="M14 6.66666H7.33333C4.38781 6.66666 2 9.05447 2 12V13.3333M14 6.66666L10 10.6667M14 6.66666L10 2.66666" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            </svg>
+          </a>
+        </div>
+      </nav>
+    </div>
+  </div>
+  
---
+++ src/lib/components/Hosting.svelte
@@ -0,0 +1,158 @@
+<script>
+
+</script>
+
+<section class="px-8 md:px-24 py-32 ">
+    <div class="flex flex-wrap -m-4">
+      <div class="w-full lg:w-1/3 p-4">
+        <p class="tracking-tight text-orange-400 font-medium mb-6">Reliable, Fast, and Secure</p>
+        <h1 class="font-heading tracking-tight text-4xl md:text-6xl font-medium mb-6 max-w-xs md:max-w-lg">Web Hosting Made Simple</h1>
+        <p class="tracking-tight text-gray-700 text-lg max-w-md mb-32">Streamline your web hosting experience with our straightforward, reliable, and secure plans.</p>
+        <div class="w-full h-px bg-gray-200 mb-14"></div>
+        <p class="tracking-tight text-lg font-semibold mb-2">
+          Protected by Cloudflare
+          <br>
+        </p>
+      </div>
+      <div class="w-full lg:w-2/3 p-4">
+        <div class="flex lg:justify-end">
+          <div class="overflow-x-auto">
+            <div class="flex gap-6">
+              <div class="w-80 flex-shrink-0">
+                <div class="rounded-2xl border border-gray-100 p-7 rounded-4xl">
+                  <p class="tracking-tight text-lg font-semibold text-center">
+                    Starter Pack
+                    <br>
+                  </p>
+                  <p class="tracking-tight text-gray-500 text-center mb-7">Best for Individuals</p>
+                  <ul class="flex flex-col gap-4 mb-8">
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">
+                        5 Websites
+                        <br>
+                      </span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="size-10">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">10 email accounts 1GB per account with customer support</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Unlimited free SSL</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Unlimited bandwidth</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Free Website Migration</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Standard DDoS Protection</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Web Apps Firewall</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Nameservers Protected by Cloudflare</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Malware Scanner</span>
+                    </li>
+                  </ul>
+                  <a href="#" class="bg-white h-16 w-full border border-gray-200 rounded-full px-5 py-4 inline-flex items-center justify-center hover:bg-gray-100 focus:bg-gray-100 focus:ring-4 focus:ring-gray-200 tracking-tight font-bold transition duration-200 text-black">Get Started Now</a>
+                </div>
+              </div>
+              <div class="w-80 flex-shrink-0">
+                <div class="rounded-2xl border border-gray-100 p-7 rounded-4xl">
+                  <p class="tracking-tight text-lg font-semibold text-center">Small Business</p>
+                  <p class="tracking-tight text-gray-500 text-center mb-7">Best for startups</p>
+                  <ul class="flex flex-col gap-4 mb-8">
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">10 Websites</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="size-10">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">25 email accounts 1GB per account with customer support</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Unlimited free SSL</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Unlimited bandwidth</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Free Website Migration</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Standard DDoS Protection</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Web Apps Firewall</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Nameservers Protected by Cloudflare</span>
+                    </li>
+                    <li class="flex items-center gap-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 20 20" fill="none" class="">
+                        <path d="M4.16667 10.8333L7.50001 14.1666L15.8333 5.83325" stroke="#22C55E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+                      <span class="tracking-tight font-medium">Malware Scanner</span>
+                    </li>
+                  </ul>
+                  <a href="#" class="bg-white h-16 w-full border border-gray-200 rounded-full px-5 py-4 inline-flex items-center justify-center hover:bg-gray-100 focus:bg-gray-100 focus:ring-4 focus:ring-gray-200 tracking-tight font-bold transition duration-200 text-black">Get Started Now</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
